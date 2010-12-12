f1c616fb99adaf910bb26a15b7759224995de5b7
Camille <roth@ehess.fr>
2010-12-13 00:50:57 +0100

    cleaned up js / css changes

--- mesr/include/header.php
+++ mesr/include/header.php
@@ -3,21 +3,18 @@ echo '<html>';
 echo '<head>';
 if (!isset($titleheader)) $titleheader="v0.2";
 if (isset($jsprotovis)) $jsheader='<script type="text/javascript" src="protovis-r3.2.js"></script>'; else $jsheader="";
-if (isset($jsjquery)) $jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>'; else $jsheader2="";
+//if (isset($jsjquery)) $jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>'; else $jsheader2="";
+$jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>';
 echo "<title>".$titleheader." [MESR/VD]</title>";
 echo '
 '.$jsheader;
 echo $jsheader2;
+
 echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
+
 echo '<link rel="stylesheet" title="Default" type="text/css" href="style.css" media="screen"/>';
 echo '<link rel="stylesheet" title="Default" type="text/css" href="style.css" media="print"/>';
-echo  '<!-- JQUERY -->';
-echo     '<script type="text/javascript" src="js/jquery/jquery-1.4.2.min.js" language="javascript"></script>';
-echo     '<!-- JQUERY UI -->';
-echo     '<script type="text/javascript" src="js/jquery-ui/jquery-ui-1.8.min.js" language="javascript"></script>';
-echo     '<!-- JQUERY UI CSS -->';
-echo     '<link type="text/css" rel="stylesheet" href="css/custom-theme/jquery-ui-1.7.3.custom.css"/>';
-echo     '<link type="text/css" rel="stylesheet" href="ex.css?3.2"/>';
+echo '<link rel="stylesheet" type="text/css" href="css/custom-theme/jquery-ui-1.7.3.custom.css"/>';
 
 echo '<meta name="author" content="CCR"/>';
 echo '</head>';
--- mesr/include/header_cluster.php
+++ mesr/include/header_cluster.php
@@ -3,14 +3,18 @@ echo '<html>';
 echo '<head>';
 if (!isset($titleheader)) $titleheader="v0.2";
 if (isset($jsprotovis)) $jsheader='<script type="text/javascript" src="protovis-r3.2.js"></script>'; else $jsheader="";
-if (isset($jsjquery)) $jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>'; else $jsheader2="";
+//if (isset($jsjquery)) $jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>'; else $jsheader2="";
+$jsheader2='<script type="text/javascript" src="jquery-1.4.4.js"></script>';
 echo "<title>".$titleheader." [MESR/VD]</title>";
 echo '
 '.$jsheader;
 echo $jsheader2;
 echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">';
+
 echo '<link rel="stylesheet" title="Default" type="text/css" href="style.css" media="screen"/>';
 echo '<link rel="stylesheet" title="Default" type="text/css" href="style.css" media="print"/>';
+echo '<link rel="stylesheet" type="text/css" href="css/custom-theme/jquery-ui-1.7.3.custom.css"/>';
+
 echo '<meta name="author" content="CCR"/>';
 
 
