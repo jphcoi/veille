6f136fb46681bc28326e229d7cb5e74c0b83f451
Camille <roth@ehess.fr>
2010-12-20 20:27:57 +0100

    correction bug popup david

--- mesr/library/fonctions_php.php
+++ mesr/library/fonctions_php.php
@@ -1069,7 +1069,7 @@ function display_billets($info_sources,$list_of_concepts,$my_period,$type_notice
 			$conc = implode("; ", convert_forme_principale_id($info_sources[$key]['concepts'][$i]));
 			//echo $conc;
 			//il faut normaliser le nom de l'index pour que javascript ne soit pas perdu
-			$index=str_replace(".","",str_replace("-","",$key."-".$i));
+			$index=str_replace("/","",str_replace(".","",str_replace("-","",$key."-".$i)));
 			$jscriptmp.="
 				$('#dialog".$index."')
 					.dialog({ autoOpen: false, stack: true, modal:true, width:600, closeOnEscape:true})
