b9672299bc50e57a9249fdcbe589ad3af2da5df4
jphcoi <jphcoi@yahoo.com>
2010-12-27 18:38:06 +0100

    normalement les dates sont correctement affichees dans les visus protovis

--- mesr/include/chart-tempo-helper.php
+++ mesr/include/chart-tempo-helper.php
@@ -1,9 +1,25 @@
 <?
 	// recuperer la variable de voisinage semantique temporel depuis la base SQL
+	$resultat=mysql_query("select term2,distances,forcemoy,direction FROM term_neighbour WHERE term1=$id_concept LIMIT 10");
+	//echo "select term2,distances,forcemoy,direction FROM term_neighbour WHERE term1=$id_concept";
+	$temp_neighborhood=mysql_fetch_array($resultat);
+	
+	$json_data ="var years = [200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318]; var dynamics= {";
+		$datef = $datef +1;
+
+	create_concept_string();
+	
+	while ($ligne=mysql_fetch_array($resultat))	
+	{
+		$json_data =$json_data.'"'.strval($liste_termes[$ligne['term2']]).'": { activity: ['.$ligne['distances'].'] },';
+	}
+	$delcaract = substr($string,0,strlen($string)-1);
+	$json_data=substr($json_data,0,strlen($json_data)-1).'};';
 	
 	$resultat=mysql_query("select idx,js FROM term_neighborhood WHERE idx=$id_concept");
 	$temp_neighborhood=mysql_fetch_array($resultat);
-	$myabove='<script type="text/javascript">'.$temp_neighborhood['js'].'</script>'.'
+//	$myabove='<script type="text/javascript">'.$temp_neighborhood['js'].'</script>'.'
+	$myabove='<script type="text/javascript">'.$json_data.'</script>'.'
 		<hr>
 		<table class=tableitems width="100%">
 		<tr valign=bottom>
@@ -69,7 +85,8 @@
 		var speriods=sx.ticks(20);
 		for(var time in speriods) {
 		   var snewDate = new Date( );
-		   snewDate.setTime((1279152000+86400*(speriods[time]-'.$dated.'))*1000);
+		   //14610 = nombre de jours écoulés entre 1er janvier 70 et 1er janvier 2010
+		   snewDate.setTime((86400*(14609+speriods[time]))*1000);
 		   sdateString = snewDate.toUTCString();
 		   sdt=sdateString.split(" ")
 		   sDateArray.push(sdt[1]+" "+sdt[2]);
