43544293a41bafc4ea6133507681274f19d6459d
Camille <roth@ehess.fr>
2010-12-30 17:44:17 +0100

    debug

--- mesr/cluster_nav_soc.php
+++ mesr/cluster_nav_soc.php
@@ -202,5 +202,167 @@ echo "</tr>";
 echo '</table>';
 
 
+
+
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+
+
+echo '<p>';
+
+echo '	
+	<table>
+	<tr>
+	<td align=left>
+	<form method="post" action="<?$_SERVER[\'PHP_SELF\']?>" >	 
+		<input type="checkbox" name="reduced" ';
+		
+if ($reduced=='1') echo ' checked="checked" '; 
+echo ' value="1" />
+	 <input type="submit" name="formSubmit" value="N\'afficher que les liens entre billets pertinents" />
+	</form>
+
+	<form method="post" action="<?$_SERVER[\'PHP_SELF\']?>" >	 
+		<input type="checkbox" name="all_periode" ';
+
+if ($all_periode=='1') echo ' checked="checked" '; 
+echo ' value="1" />
+	 <input type="submit" name="formSubmit" value="Afficher les liens enregistrés sur l\'ensemble des périodes" />
+	</form>';
+	
+echo '</td>';
+	
+	
+echo "<td align=right class=tableitems><i>seuil de pertinence:</i>";
+
+$old_url  =$_SERVER['REQUEST_URI'];
+$old_urlv = explode('MESR_bac/',$old_url);
+if (count($old_urlv)>0)
+	{$old_url = $old_urlv[1];}
+$old_urls = explode('&pertinence=',$old_url);
+$new_url=$old_urls[0];//.'&pertinence=';
+
+echo '<form action="'.$new_url.'" method="get" style="display:inline;">';
+echo '<select name="pertinence">';
+	echo '<option value=10';
+	if ($pertinence==0.1) echo(" selected");
+	echo '>';
+	echo "minimal (>10%)";
+	echo '</option>';
+
+	echo '<option value=20';
+	if ($pertinence==0.2) echo(" selected");
+	echo '>';
+	echo "faible (>20%)";
+	echo '</option>';
+
+	echo '<option value=30';
+	if ($pertinence==0.3) echo(" selected");
+	echo '>';
+	echo "médian (>30%)";
+	echo '</option>';
+
+	echo '<option value=40';
+	if ($pertinence==0.4) echo(" selected");
+	echo '>';
+	echo "fort (>40%)";
+	echo '</option>';
+
+echo '</select>';
+echo '<input type="hidden" value="'.$id_cluster.'" name="id_cluster">';
+echo '<input type="hidden" value="'.$nav.'" name="nav">';
+echo '<input type="hidden" value="'.$periode.'" name="periode">';
+
+
+echo '<input type="submit" value="Modifier le seuil de pertinence">';
+echo '</form>';
+
+
+
+
+$result=mysql_query($query);
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+
+
+echo '<table class=tableitems width=100% cellspacing=0 cellpadding=1 style="font-variant:small-caps;">';
+echo '<tr>';
+	
+echo "<td align=left width=40%><i>les 5 billets les plus pertinents</i></td>";
+
+echo "</tr>";
+echo '</table>';
+
+
+echo "<table class=tableitems rules=all width=100% cellspacing=0 cellpadding=0>";
+echo '<tr>';
+echo "<td align=left width=100%>";
+		
+if ($affichage>0){
+
+	//COHERENT:
+	//				$commande_sql_pert = "SELECT id_billet,overlap_size,billet_size from biparti where cluster = '".$id_cluster."' AND periode = '".derange_periode($my_period)."' AND overlap_size/cluster_size/log10(10+billet_size-overlap_size)>=".$pertinence.' and overlap_size/cluster_size>0.1 LIMIT 5';
+	//TOP 5 tout le temps:
+	$commande_sql_pert = "SELECT id_billet,overlap_size,billet_size from biparti where cluster = '".$id_cluster."' AND periode = '".derange_periode($my_period)."' ORDER BY overlap_size/cluster_size/log10(10+billet_size-overlap_size) DESC LIMIT 5";//.$pertinence.' and overlap_size/cluster_size>0.1 LIMIT 5';
+	//ANCIEN
+	//			$commande_sql_pert = "SELECT id_billet,overlap_size,billet_size from biparti where cluster = '".$id_cluster."' AND periode = '".derange_periode($my_period)."'".' and overlap_size/cluster_size>0.15'.' ORDER BY overlap_size/cluster_size/log10(10+billet_size-overlap_size) DESC LIMIT 5';
+	
+	$res_temp = mysql_query($commande_sql_pert);
+	$liste_of_posts=array();
+	$liste_of_size=array();
+	while ($row = mysql_fetch_array ($res_temp))
+	{
+		$liste_of_posts[$row['id_billet']]=$row['overlap_size'];
+		$liste_of_size[$row['id_billet']]=$row['billet_size'];
+	}
+	
+	$nb_termes_list = $liste_of_posts;
+	$nb_size_list = $liste_of_size;
+	$resultat = extract_permalink(array_keys($liste_of_posts));
+	$i=0;
+	$info_sources=array();
+	while( $row = mysql_fetch_array ($resultat))
+	{
+		$perma=$row['permalink'];
+		$site=strip_www($row['site']);
+		$site =str_replace('***','; ',$site);
+		$id = $row['id'];
+		$nb_terme=$nb_termes_list[$id];
+		$nb_size=$nb_size_list[$id];
+		$idauteur=$row['auteur_id']; 
+		$content=$row['content'];
+		$concepts=$row['concepts_id'];
+		if (!array_key_exists($site,$info_sources)) {
+		$info_sources[$site]=array('site'=>$site,'idauteur'=>$idauteur,'permaliens'=>array(),'titres'=>array(),'dates'=>array(),'nbtermes'=>array(),'nbsize'=>array());
+		}
+		$info_sources[$site]['permaliens'][]=$perma;
+		$info_sources[$site]['titres'][]=clean_text(str_replace('popostrophe',"'",$row['title']));
+		if (strlen($row['jours'])>3)
+		{$info_sources[$site]['dates'][]=$row['jours'];}
+		else
+		{$info_sources[$site]['dates'][]=adjust_date_jours($row['jours']);}
+		$info_sources[$site]['nbtermes'][]=$nb_terme;
+		$info_sources[$site]['nbsize'][]=$nb_size;
+		$info_sources[$site]['content'][]=$content;
+		$info_sources[$site]['concepts'][]=$concepts;
+
+		$i++;
+	}
+
+	uksort($info_sources,"strcasecmpcam");
+	
+	display_billets($info_sources,$list_of_concepts,$my_period,$type_notice);
+	
+	}
+else
+	{
+	echo "aucun billet pertinent";
+	}
+	
+echo "</td></tr></table>";
 	
 ?>
