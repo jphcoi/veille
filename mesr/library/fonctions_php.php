4840a857348c9504a62ee0c233e214862f785dd5
Camille <roth@ehess.fr>
2010-12-30 17:51:48 +0100

    debug general

--- mesr/library/fonctions_php.php
+++ mesr/library/fonctions_php.php
@@ -1072,12 +1072,8 @@ function display_billets($info_sources,$list_of_concepts,$my_period,$type_notice
 	$backdark="#DDDDDD";
 	$backdarker="#CCCCCC";
 	
-// 	echo "<table class=tableitems rules=all width=100% cellspacing=0 cellpadding=0>";
-// 	echo "<tr>";
-// 	echo "<td align=left width=80%>";
-	echo "<table width=100% cellspacing=0 cellpadding=0>";
+	echo "<table class=tableitems width=100% cellspacing=0 cellpadding=0>";
 	$odd=0;
-	//$jscriptmp="";
 	foreach(array_keys($info_sources) as $key)
 	{
 		//print_r($info_sources[$key]['dates']);
@@ -1141,12 +1137,7 @@ function display_billets($info_sources,$list_of_concepts,$my_period,$type_notice
 		echo "</td>";
 		echo "</tr>";
 	}
-//	echo "</table>";
 	echo "</table>";
-	//echo '
-	//<script> $(function() { '.$jscriptmp.' 
-	//	});</script>';
-
 }
 					
 ////////////
