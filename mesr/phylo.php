9d57c84746f3ad057b459e1e990f26085f22d6f3
Camille <roth@ehess.fr>
2011-01-04 03:40:09 +0100

    forme phylo

--- mesr/phylo.php
+++ mesr/phylo.php
@@ -226,8 +226,8 @@ function branch_list_string($mysql_branch_list,$depth,$min_similarity){
 		
 				$branch='<tr onMouseOver="this.style.backgroundColor=\''.$whitedarker.'\';" onMouseOut="this.style.backgroundColor=\''.$whitedark.'\';">';
 				$branch.='<td width=50px style="text-align:right;font-size: x-small;">'.$ligne['nb_fields'].'&nbsp;champs</td>';
-				$branch.='<td width=50px style="font-size: x-small;">';
-				$branch.=str_replace("-",'</td><td width=50px style="font-size: x-small;">',str_replace(" ","&nbsp;","  ".get_short_string_periode($ligne['first_period'].'-'.$ligne['last_period'])." "));
+				$branch.='<td width=50px style="font-size: x-small; text-align:right;">du&nbsp;';
+				$branch.=str_replace("-",'</td><td width=50px style="font-size: x-small; text-align:right;">&nbsp;au&nbsp;',str_replace(" ","&nbsp;","  ".get_short_string_periode($ligne['first_period'].'-'.$ligne['last_period'])." "));
 				$branch.='</td><td>';
 				$branch.='<i><a href="cluster.php?id_cluster='.$branch_list[$index]['branch_last_period_cluster_id'].'&periode='.str_replace(' ','-',$branch_list[$index]['branch_last_period']).'">';
 				$branch.=ucfirst($branch_list[$index]['label']).'</a></i>';
@@ -259,8 +259,8 @@ function branch_list_string($mysql_branch_list,$depth,$min_similarity){
 			
 			$branch='<tr onMouseOver="this.style.backgroundColor=\''.$whitedarker.'\';" onMouseOut="this.style.backgroundColor=\''.$whitedark.'\';">';
 			$branch.='<td width=50px style="font-size: x-small;">'.$ligne['nb_fields'].'&nbsp;champs</td>';
-			$branch.='<td width=50px style="font-size: x-small;">';
-			$branch.=str_replace("-",'</td><td width=50px style="font-size: x-small;">',str_replace(" ","&nbsp;","  ".get_short_string_periode($ligne['first_period'].'-'.$ligne['last_period'])." "));
+			$branch.='<td width=50px style="font-size: x-small; text-align:right;">du&nbsp;';
+			$branch.=str_replace("-",'</td><td width=50px style="font-size: x-small; text-align:right;">&nbsp;au&nbsp;',str_replace(" ","&nbsp;","  ".get_short_string_periode($ligne['first_period'].'-'.$ligne['last_period'])." "));
 			$branch.='</td><td onMouseOver="this.style.backgroundColor=\''.$whitedarker.'\';" onMouseOut="this.style.backgroundColor=\''.$whitedark.'\';">';
 			$branch.='<a href="cluster.php?id_cluster='.$branch_list[$index_grouped[0]]['branch_last_period_cluster_id'].'&periode='.str_replace(' ','-',$branch_list[$index_grouped[0]]['branch_last_period']).'">';
 			$branch.=ucfirst($branch_list[$index_grouped[0]]['label']).'</a>';
