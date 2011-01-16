d78cc1936398e04f33a9dd2a0d3ee57a35a6e95d
davidchavalarias <david.chavalarias@polytechnique.edu>
2011-01-16 13:29:08 +0100

    lien star dans cluster.php

--- mesr/cluster.php
+++ mesr/cluster.php
@@ -305,7 +305,7 @@ $query="select * FROM partitions WHERE id_partition=".$id_partition;
 $resultat=mysql_query($query) or die ("<b>Requête non exécutée (récupération de info de partition)</b>.");
 $partition_infos=mysql_fetch_array($resultat);
 list($jscriptmp,$linkFilThematique)=linkFilThematique($jscriptmp,$id_partition,$partition_infos,$backdarker);
-list($jscriptmp,$linkstar)=linkstar($jscriptmp,$partition_infos[id_partition],$partition_infos,$periode);
+list($jscriptmp,$linkstar)=linkstar($jscriptmp,$partition_infos[id_partition],$partition_infos,str_replace('-',' ', $periode));
 list($jscriptmp,$linkcluster)=link2clusters($jscriptmp,$partition_infos[id_partition],
         $partition_infos,str_replace('-',' ', $periode));
 
