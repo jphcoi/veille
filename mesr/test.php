148a9b653165093095975c387a2d345f71a5266b
David Chavalarias <david.chavalarias@polytechnique.edu>
2013-01-22 18:34:35 +0100

    fichier test

---
+++ mesr/test.php
@@ -0,0 +1,16 @@
+<?
+//include("login_check.php");
+include("library/fonctions_php.php");
+include("parametre.php");
+$raphael=TRUE;
+include("include/header.php");
+include("banner.php");
+
+echo 'toto';
+
+include("footer.php");
+
+?>
+
+</body>
+</html>
